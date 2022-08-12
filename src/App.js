f69fca18cb8eb88467df51634c7dba800dc99a34
Romil Modi <77897274+romil19@users.noreply.github.com>
2022-08-12 18:00:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
             <Footer/>
           </Route>
           <Route exact path="/">
-            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
+            <TextForm showAlert={showAlert} heading="Try Text-O-Buster - word counter, character counter, remove extra spaces" mode={mode}/>
           </Route>
     </Switch>
     </Router>
